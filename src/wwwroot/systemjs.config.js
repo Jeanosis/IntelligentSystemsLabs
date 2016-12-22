5932d698d7b33c742ba199d42b3bec51a0a75992
Jeanosis <plaits123@gmail.com>
2016-12-22 03:40:57 +0200

    #11 .gitignore updated, jquery and tether included, app component template separated to app.component.html

--- src/wwwroot/systemjs.config.js
+++ src/wwwroot/systemjs.config.js
@@ -24,7 +24,7 @@
         // packages tells the System loader how to load when no filename and/or no extension
         packages: {
             app: {
-                main: './main.js',
+                main: '../main.js',
                 defaultExtension: 'js'
             },
             rxjs: {
