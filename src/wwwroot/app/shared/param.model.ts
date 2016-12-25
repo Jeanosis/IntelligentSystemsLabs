9b31fae700eb7828ba0add543f15238fb6047fa6
Jeanosis <plaits123@gmail.com>
2016-12-26 01:34:15 +0200

    #17 parameters Class added to models

--- src/wwwroot/app/shared/param.model.ts
+++ src/wwwroot/app/shared/param.model.ts
@@ -1,18 +1,27 @@
+import { Class } from './class.model';
 /*export interface InputParam {
     name: string;
     from: number;
     to: number;
 }*/
 export class Param {
-    constructor(options: { name?: string, value?: number, from?: number, to?: number } = {}) {
+    constructor(options: {
+        name?: string,
+        value?: number,
+        from?: number,
+        to?: number,
+        classes?: Class[]
+    } = {}) {
         this.name = options.name;
         this.value = options.value;
         this.from = options.from || 0;
         this.to = options.to || 100;
+        this.classes = options.classes || [];
     }
 
     name: string;
     value: number;
     from: number;
     to: number;
+    classes: Class[];
 }
