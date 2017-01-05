49c7ee093077a455f4a6d5b0d32b0ee650c89e18
Igor <harry.tallbelt@gmail.com>
2017-01-05 18:41:57 +0200

    #37 Added from field to Grapg class.

--- src/Models/Graph.cs
+++ src/Models/Graph.cs
@@ -5,6 +5,7 @@ namespace WebApplication.Models
     public class Graph
     {
         public List<double> values { get; set; }
+        public double from { get; set; }
         public double step { get; set; }
     }
 }
